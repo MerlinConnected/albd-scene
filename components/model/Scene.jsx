6b058e2e249f254f14f0d4320ee748088df0d063
Gaëtan Jestin <gaetanjestin@outlook.com>
2024-02-02 17:52:12 +0100

    feat: camera animation on load

--- components/model/Scene.jsx
+++ components/model/Scene.jsx
@@ -1,40 +1,51 @@
-import { Perf } from 'r3f-perf'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { OrbitControls, Loader, FlyControls } from '@react-three/drei'
+import { Canvas, useFrame, useThree } from '@react-three/fiber'
+import { Suspense, useRef, useState, useEffect } from 'react'
 import * as easing from 'maath/easing'
-
-import { Suspense } from 'react'
+import * as THREE from 'three'
 
 import Model from './Model'
 import Lights from './Lights'
 import FollowLight from './FollowLight'
 
 function Rig() {
-	useFrame((state, delta) => {
-		easing.damp3(
-			state.camera.position,
-			[Math.sin(-state.pointer.x), state.pointer.y, 50 + Math.cos(state.pointer.x)],
-			0.2,
-			delta
-		)
-		state.camera.lookAt(0, 0, 0)
+	const { camera } = useThree()
+	const initialPosition = useRef(new THREE.Vector3(0, -1.5, 75))
+	const finalPosition = useRef(new THREE.Vector3(0, 0, 50))
+	const [hasAnimated, setHasAnimated] = useState(false)
+
+	const empty = new THREE.Object3D()
+
+	useFrame((state) => {
+		if (!hasAnimated) {
+			camera.position.lerp(initialPosition.current.lerp(finalPosition.current, 0.05), 0.1)
+
+			camera.lookAt(0, 0, 0)
+			// Check if the camera is close to the final position
+			if (camera.position.distanceTo(finalPosition.current) < 0.1) {
+				setHasAnimated(true)
+			}
+		} else {
+			easing.damp3(
+				state.camera.position,
+				[Math.sin(-state.pointer.x), state.pointer.y, 50 + Math.cos(state.pointer.x)],
+				0.2
+			)
+			state.camera.lookAt(0, 0, 0)
+		}
 	})
 }
 
 export default function Scene() {
 	return (
 		<>
-			<Canvas shadows camera={{ fov: 10, position: [0, -1.5, 50] }}>
+			<Canvas shadows camera={{ fov: 10, position: [0, -1.5, 75] }}>
 				<Suspense fallback={null}>
 					<Model />
-					{/* <OrbitControls makeDefault /> */}
 					<Lights />
 					<FollowLight />
-					{/* <Perf /> */}
 					<Rig />
 				</Suspense>
 			</Canvas>
-			{/* <Loader /> */}
 		</>
 	)
 }
