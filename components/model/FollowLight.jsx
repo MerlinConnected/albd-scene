6394f2f068be621333c2b52c1927aff561c7cd0f
Gaëtan Jestin <gaetanjestin@outlook.com>
2024-02-02 17:57:01 +0100

    fix(pointer): the light can no longer pass through walls (kinda)

--- components/model/FollowLight.jsx
+++ components/model/FollowLight.jsx
@@ -1,14 +1,18 @@
 import { useFrame, useThree } from '@react-three/fiber'
-import { useState, useRef } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import { Vector3 } from 'three'
 
 export default function FollowLight() {
 	const lightRef = useRef()
+	const gcRef = useRef() // Ground collider reference
+	const wcRef = useRef() // Wall collider reference
+
 	const { camera } = useThree()
 	const [targetPosition, setTargetPosition] = useState(new Vector3())
+	const [canMoveX, setCanMoveX] = useState(true)
+	const [canMoveY, setCanMoveY] = useState(true)
 
 	useFrame((state) => {
-		// Adjust the z-depth as needed
 		const zDepth = -3
 		const vector = new Vector3(state.pointer.x, state.pointer.y, 0.5)
 		vector.unproject(camera)
@@ -16,11 +20,27 @@ export default function FollowLight() {
 		const distance = (zDepth - camera.position.z) / dir.z
 		const pos = camera.position.clone().add(dir.multiplyScalar(distance))
 
-		setTargetPosition(pos)
+		const groundPos = gcRef.current.position
+		const wallPos = wcRef.current.position
 
-		// Interpolate position
-		const alpha = 0.1 // Adjust alpha for speed of interpolation
+		if (pos.y > groundPos.y) {
+			setCanMoveY(true)
+		} else {
+			setCanMoveY(false)
+		}
+
+		if (pos.x < wallPos.x) {
+			setCanMoveX(true)
+		} else {
+			setCanMoveX(false)
+		}
+
+		setTargetPosition(new Vector3(canMoveX ? pos.x : targetPosition.x, canMoveY ? pos.y : targetPosition.y, pos.z))
+
+		const alpha = 0.1
 		lightRef.current.position.lerp(targetPosition, alpha)
+		// lightRef.current.position.x = targetPosition.x
+		// lightRef.current.position.y = targetPosition.y
 	})
 
 	return (
@@ -32,6 +52,15 @@ export default function FollowLight() {
 				</mesh>
 				<pointLight position={[0, 0, 0]} intensity={10} />
 			</group>
+			<mesh ref={gcRef} rotation={[-Math.PI / 2, 0, 0]} position={[0, -3.4, 0]}>
+				<planeGeometry args={[10, 10]} />
+				<meshStandardMaterial transparent opacity={0} color={'hotpink'} />
+			</mesh>
+
+			<mesh ref={wcRef} rotation={[0, -Math.PI / 2, 0]} position={[5.3, 0, 0]}>
+				<planeGeometry args={[10, 10]} />
+				<meshStandardMaterial transparent opacity={0} color={'cyan'} />
+			</mesh>
 		</>
 	)
 }
