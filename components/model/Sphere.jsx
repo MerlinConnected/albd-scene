97439dfd2b7d6f965f91ce36120a15c8891476dd
Gaëtan Jestin <gaetanjestin@outlook.com>
2024-03-25 14:34:38 +0100

    fix(Sphere): roughness & specular values

--- components/model/Sphere.jsx
+++ components/model/Sphere.jsx
@@ -117,7 +117,7 @@ function Sphere() {
 	return (
 		<mesh ref={meshRef} scale={0.45}>
 			<icosahedronGeometry ref={geoRef} args={[1, 96]} />
-			<meshPhysicalMaterial ref={materialRef} color='#242424' specularIntensity={0.33} />
+			<meshPhysicalMaterial ref={materialRef} color='#353535' specularIntensity={0.1} roughness={1} />
 		</mesh>
 	)
 }
